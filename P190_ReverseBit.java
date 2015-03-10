df59cb997866c8c57a4a2cf1924317ce8d10deff
bluescindy <bluescindy@users.noreply.github.com>
2015-03-10 12:58:54 -0700

    Update P190_ReverseBit.java

--- P190_ReverseBit.java
+++ P190_ReverseBit.java
@@ -1,13 +1,16 @@
 
 public class ReverseBit {
 	public static int reverseBits(int n) {
+		 // In java, int type is always signed, right shift is 
+        	// signed shift by default, however, we need a unsigned right
+        	// shift in this problem.
 		if(n == 0) return n;
-		n = (n & 0xffff0000) >> 16 | (n & 0x0000ffff) << 16;
-		n = (n & 0xff00ff00) >> 8 | (n & 0x00ff00ff) << 8;
-		n = (n & 0xf0f0f0f0) >> 4 | (n & 0x0f0f0f0f) << 4;
-		n = (n & 0xcccccccc) >> 2 | (n & 0x33333333) << 2;
-		n = (n & 0xaaaaaaaa) >> 1 | (n & 0x55555555) << 1;
-		return n;
+		n = ((n & 0xFFFF0000) >>> 16) | ((n & 0xFFFF) << 16);
+		n = ((n & 0xFF00FF00) >>> 8) | ((n & 0x00FF00FF) << 8);
+        	n = ((n & 0xF0F0F0F0) >>> 4) | ((n & 0x0F0F0F0F) << 4);
+        	n = ((n & 0xCCCCCCCC) >>> 2) | ((n & 0x33333333) << 2);
+        	n = ((n & 0xAAAAAAAA) >>> 1) | ((n & 0x55555555) << 1);
+        	return n;
 	}
 
 	public static void main(String[] args) {
