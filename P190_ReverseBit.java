832c156f9d99482e3692c90acc8606d43d122503
bluescindy <bluescindy@users.noreply.github.com>
2015-03-09 21:28:30 -0700

    Update P190_ReverseBit.java

--- P190_ReverseBit.java
+++ P190_ReverseBit.java
@@ -5,8 +5,8 @@ public class ReverseBit {
 		n = (n & 0xffff0000) >> 16 | (n & 0x0000ffff) << 16;
 		n = (n & 0xff00ff00) >> 8 | (n & 0x00ff00ff) << 8;
 		n = (n & 0xf0f0f0f0) >> 4 | (n & 0x0f0f0f0f) << 4;
-		n = (n & 0xc3c3c3c3) >> 2 | (n & 0x3c3c3c3c) << 2;
-		n = (n & 0xa5a5a5a5) >> 1 | (n & 0x5a5a5a5a) << 1;
+		n = (n & 0xcccccccc) >> 2 | (n & 0x33333333) << 2;
+		n = (n & 0xaaaaaaaa) >> 1 | (n & 0x55555555) << 1;
 		return n;
 	}
 
